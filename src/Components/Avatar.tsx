b64a49cd3fa00ccdbe1ca024fba4e71e88f91ec6
lkavila <lkavila@uninorte.edu.co>
2022-04-09 19:02:01 -0500

    delete usless prop

--- src/Components/Avatar.tsx
+++ src/Components/Avatar.tsx
@@ -1,6 +1,6 @@
 //import UserImg from "../assets/img/default-user.png"
 const Avatar = (props: any) => {
-    const { alt = 'avatar', size = '12', className, ...rest } = props
+    const { size = '12', className, ...rest } = props
     return (
         <div {...rest} className={`rounded-full h-${size} w-${size} bg-azul3 flex items-center justify-center hover:filter hover:brightness-75 ${className} `}>
         </div>
